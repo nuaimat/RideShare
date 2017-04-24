6f77e0f91d237209f137bb6e393f6437921960f9
Mo Nuaimat <nuaimat@gmail.com>
2017-04-24 12:26:43 -0500

    creating sample rea

--- src/main/java/edu/mum/wap42016/group1/project/RootServlet.java
+++ src/main/java/edu/mum/wap42016/group1/project/RootServlet.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 public class RootServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
         resp.getWriter().println("Welcome home");
     }
 
