f79f64c4298c84ae450f2ff323a9c181bf634caa
Mo Nuaimat <nuaimat@gmail.com>
2017-04-24 12:30:19 -0500

    form basic temp arch

--- src/main/java/edu/mum/wap42016/group1/project/RootServlet.java
+++ src/main/java/edu/mum/wap42016/group1/project/RootServlet.java
@@ -15,7 +15,8 @@ import java.io.IOException;
 public class RootServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Welcome home");
+        //resp.getWriter().println("Welcome home");
+        req.getRequestDispatcher("/main.jsp").forward(req, resp);
     }
 
     @Override
--- src/main/webapp/main.jsp
+++ src/main/webapp/main.jsp
@@ -1,4 +1,3 @@
 <jsp:include page="templates/header_template.jsp" />
 <h2>Hello World!</h2>
-</body>
-</html>
+<jsp:include page="templates/footer_template.jsp" />
