e8130bd7bc695f416abb400be36f37ad55193055
sami kidanu <samimulaw@gmail.com>
2017-04-24 23:23:16 -0500

    styled register form

--- src/main/java/edu/mum/wap42016/group1/project/dao/UserDAO.java
+++ src/main/java/edu/mum/wap42016/group1/project/dao/UserDAO.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 
 import edu.mum.wap42016.group1.project.model.User;
 
-public class Users {
-	
-	
+public class UserDAO {
+	User user;
 	void addToDB(User user){
-		 
+		 this.user=user;
 	 }
 	void getUSer(){
 		
